80e8f2376e9cd37fe235dcf84c9177a641909bab
shouston3 <shouston3@me.com>
2018-01-26 10:43:26 +0000

    follow heroku phoenix setup instructions

--- lib/languages_example_web/channels/user_socket.ex
+++ lib/languages_example_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule LanguagesExampleWeb.UserSocket do
   # channel "room:*", LanguagesExampleWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
